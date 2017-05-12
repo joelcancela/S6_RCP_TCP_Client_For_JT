fddcab1b219a14bad490d05b0048d919a34259e0
Nassim Bounouas <NassimBounouas@users.noreply.github.com>
2017-05-12 11:35:23 +0200

    Flux changed, trying to send to serve

--- client_for_jt/src/main/java/boucan/InnovClientImplTCP.java
+++ client_for_jt/src/main/java/boucan/InnovClientImplTCP.java
@@ -1,8 +1,7 @@
 package boucan;
 
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.List;
 import java.util.Scanner;
@@ -11,9 +10,11 @@ import java.util.Scanner;
  * Created by Nassim B on 04/05/17.
  */
 public class InnovClientImplTCP extends InnovClient {
-    private Socket socket; // Socket de connexion au client
-    private ObjectOutputStream objectOutputStream; // Flux de sortie vers le client
-    private ObjectInputStream objectInputStream;   // Flux d'entrée depuis le client
+    private Socket socket; // Socket de connexion au serveur
+    //private ObjectOutputStream objectOutputStream; // Flux de sortie vers le client
+    private PrintWriter out;
+    //private ObjectInputStream objectInputStream;   // Flux d'entrée depuis le client
+    private BufferedReader in;
     private Scanner scanner = new Scanner(System.in);
     private String input;
     private List<String> ideas;
@@ -47,16 +48,26 @@ public class InnovClientImplTCP extends InnovClient {
 
     protected void connect() {
         try {
+            System.out.println("IP : " + this.ip);
+            System.out.println("Port : " + this.port);
             this.socket = new Socket(this.ip, this.port);
             System.out.println("Connected to "+ip+":"+port);
-            this.objectOutputStream = new ObjectOutputStream(this.socket.getOutputStream());
-            this.objectInputStream = new ObjectInputStream(this.socket.getInputStream());
-            try {
-                System.out.println(((String)objectInputStream.readObject()));
-            } catch (ClassNotFoundException e) {
-            }
+            //this.objectOutputStream = new ObjectOutputStream(this.socket.getOutputStream());
+            this.out = new PrintWriter(socket.getOutputStream(),true);
+            //this.objectInputStream = new ObjectInputStream(this.socket.getInputStream());
+            this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            //    System.out.println(((String)objectInputStream.readObject()));
+                this.out.write(new String("AINI 1.0\n" +
+                        "ADD IDEA\n" +
+                        "BEGIN\n" +
+                        "{\"desc\":\"desc idea\", \"idStudent\":\"idStudent\",\"mailStudent\":\"email\",\"idIdea\":\"IDidea\"}\n" +
+                        "END"));
+            System.out.println(this.in.readLine());
+
             System.out.printf("###> ");
         } catch (IOException e) {
+            e.printStackTrace();
             System.out.println("Connection refused");
         }
     }
@@ -70,11 +81,12 @@ public class InnovClientImplTCP extends InnovClient {
         while (this.getInput() != null) {
 
             try {
-                toSend = this.parseInput();
-                this.objectOutputStream.writeObject(toSend);
-                this.processResponse();
-                if (toSend instanceof String) break;
-                System.out.printf("###>");
+               // toSend = this.parseInput();
+                this.out.write(this.input);
+                System.out.println(this.in.readLine());
+                //this.processResponse();
+                //if (toSend instanceof String) break;
+                //System.out.printf("###>");
             } catch (UnrecognizedCommandException e) {
                 System.out.println("Sorry, this command is undefined");
                 System.out.printf("###>");
