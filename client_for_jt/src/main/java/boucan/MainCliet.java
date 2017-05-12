8f1aafcfa6aa39c6d60208820755a2f1dee928cf
Nassim Bounouas <NassimBounouas@users.noreply.github.com>
2017-05-12 16:00:33 +0200

    Add added, listing failed

--- client_for_jt/src/main/java/boucan/MainCliet.java
+++ client_for_jt/src/main/java/boucan/MainCliet.java
@@ -11,21 +11,38 @@ import java.util.Scanner;
  * Created by Nassim B on 12/05/17.
  */
 public class MainCliet {
+
+    protected static String inputProject() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("\tProject Name : ");
+        String name = scanner.nextLine();
+        System.out.printf("\tCreator Name : ");
+        String creator = scanner.nextLine();
+        System.out.printf("\tCreator Mail : ");
+        String mail = scanner.nextLine();
+        System.out.printf("\tProject Description : ");
+        String description = scanner.nextLine();
+
+        return "AINI 1.0\nADD IDEA\nBEGIN\n{\"desc\":" + description + ", \"idStudent\":" + creator + ",\"mailStudent\":" + mail + ",\"idIdea\":" + name + "}\nEND";
+    }
+
     public static void main(String[] args) {
         try {
             Socket socket = new Socket("localhost", 8080);
             PrintWriter out = new PrintWriter(socket.getOutputStream());
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             Scanner scan = new Scanner(System.in);
-            String toSend;
+            String action;
             System.out.println("##>");
-            while ((toSend = scan.next()) != null) {
-                out.println(toSend);
-                out.println(new String("AINI 1.0\n"+
-                "ADD IDEA\n"+
-                "BEGIN\n"+
-                "{\"desc\":\"desc idea\", \"idStudent\":\"idStudent\",\"mailStudent\":\"email\",\"idIdea\":\"IDidea\"}\n" +
-                "END\n"));
+            while ((action = scan.next()) != null) {
+                switch (action) {
+                    case "add" :
+                        out.println(MainCliet.inputProject());
+                        break;
+                    case "list" :
+                        out.println("AINI 1.0\nDISP IDEA\nBEGIN\n{ }\nEND");
+                        break;
+                }
                 out.flush();
                 System.out.println("##>");
             }
